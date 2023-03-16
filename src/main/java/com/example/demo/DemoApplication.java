2cc0c7c24528cd61c1d7404608e2d794d51cfc75
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-16 14:16:23 +0200

    add SocialControler

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-
-	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
