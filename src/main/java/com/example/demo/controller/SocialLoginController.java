fae99fd75fd9c7757f4a97af161c71cbd6ada2f0
omer358 <omermaki358@gmail.com>
2023-03-23 13:38:23 +0200

    get google  authenticated user info

--- src/main/java/com/example/demo/controller/SocialLoginController.java
+++ src/main/java/com/example/demo/controller/SocialLoginController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
 @RequestMapping
-public class SocialLoginController {
+public class  SocialLoginController {
     @GetMapping
     public String getHomePage() {
         return "index";
+    }
+
+    @GetMapping("/user/me")
+    public Map<String, Object> userDetails(@AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes();
     }
 }
